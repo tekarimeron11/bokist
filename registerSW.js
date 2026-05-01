5b4da002f3f6676340ea2ca944a360a3eba13bc3
tekarimeron11 <tekarimeron11@users.noreply.github.com>
2026-05-01 19:43:51 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/bokist/sw.js', { scope: '/bokist/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/luca/sw.js', { scope: '/luca/' })})}
